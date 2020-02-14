940cd28bc411ca083238025be1f3dbe6d6548d1d
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-14 00:26:32 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(str)
   string_to_array = str.split(" ")
-  p string_to_array.collect{|word| word.reverse}
+  string_to_array.collect{|word| word.reverse}
   string_to_array.join(' ')
 end

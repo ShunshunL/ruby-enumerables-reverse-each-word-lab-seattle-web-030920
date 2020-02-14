3ff5558e4c8634616322bc03f8867a55ba1358ae
Sophia Lyu <shunshunlyu@protonmail.com>
2020-02-14 00:19:43 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
 def reverse_each_word(str)
   string_to_array = str.split(" ")
-  return_str << string_to_array.each{|word| }
+  return_str << string_to_array.each{|word| word.reverse_each_word}
 end
